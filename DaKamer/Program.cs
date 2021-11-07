5c5f5940156365bfd2973d5b9861dc7ad5792df8
GeleTrui <70946651+GeleTrui@users.noreply.github.com>
2021-11-07 23:15:18 +0100

    Add files via upload

---
+++ DaKamer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Kamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kamer room1 = new Kamer(5, 5);
+        }
+    }
+
+    class Tile
+    {
+
+    }
+    class Kamer
+    {
+        Tile[][] TileMap;
+
+        public Kamer(int width, int height)
+        {
+            TileMap = new Tile[height][];
+            for (int i = 0; i < TileMap.Length; i++)
+            {
+                TileMap[i] = new Tile[width];
+                for (int j = 0; j < TileMap[i].Length; j++)
+                {
+
+                    TileMap[i][j] = new Tile();
+
+                }
+            }
+        }
+    }
+}
